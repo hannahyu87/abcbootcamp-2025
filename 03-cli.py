b9654f86ca7033c583533cf9ba59b07a20063e2c
Hannah Yu <hannahyu87@naver.com>
2025-07-28 11:55:38 +0900

    git 실습

---
+++ 03-cli.py
@@ -0,0 +1,17 @@
+from http.client import LineTooLong
+from ai import get_personality_analysis
+
+load_dotenv()
+
+print("#AI 관상가입니다. 얼굴 특징을 입력해주시면 성격과 미래를 전망해드릴게요.")
+line = input("얼굴 특징").strip()
+
+if not Line:
+    print("에러: 얼굴 특징을 입력하지 않으셨습니다.")
+else:
+    print("입력하신 얼굴 특징 :",line)
+    print("분석중...")
+    result = get_personality_analysis(line)
+    print("분석 완료!")
+    print(result)
+#get_personality_analysis()
